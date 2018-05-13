f2368b5d951ae9e8c43cbcb9f9aaa6cc1b9072ed
anh-khue <thailyanhkhue@gmail.com>
2018-05-13 13:14:45 +0700

    Define input stream binding.

--- src/main/java/io/anhkhue/kafkawebanalytics/AnalyticsBinding.java
+++ src/main/java/io/anhkhue/kafkawebanalytics/AnalyticsBinding.java
@@ -1,5 +1,7 @@
 package io.anhkhue.kafkawebanalytics;
 
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.stereotype.Component;
@@ -8,6 +10,10 @@ import org.springframework.stereotype.Component;
 public interface AnalyticsBinding {
 
     String PAGE_VIEWS_OUT = "pageViewsOut";
+    String PAGE_VIEWS_IN = "pageViewsIn";
+
+    @Input(PAGE_VIEWS_IN)
+    KStream<String, PageViewEvent> pageViewsIn();
 
     @Output(PAGE_VIEWS_OUT)
     MessageChannel pageViewsOut();
