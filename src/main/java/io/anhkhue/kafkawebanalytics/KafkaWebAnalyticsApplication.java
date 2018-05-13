903f64e814488e5dd89b0a13e433e04aeec014b2
anh-khue <thailyanhkhue@gmail.com>
2018-05-13 10:00:09 +0700

    Init project.

---
+++ src/main/java/io/anhkhue/kafkawebanalytics/KafkaWebAnalyticsApplication.java
@@ -0,0 +1,12 @@
+package io.anhkhue.kafkawebanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWebAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWebAnalyticsApplication.class, args);
+	}
+}
